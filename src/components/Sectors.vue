21e8882eec746a4e968d95bf2a2190c31519069c
Ian Boyle <irboyle21@gmail.com>
2021-12-27 09:44:00 -0700

    uses axios to fetch data from backend and renders data using props

---
+++ src/components/Sectors.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div v-for="sector in sectors" :key="sector.id">{{ sector.sector }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sectors: { tpye: Array },
+  },
+};
+</script>
--- src/views/Parent.vue
+++ src/views/Parent.vue
@@ -1,21 +1,31 @@
 <template>
   <div>
-    <!-- <Child :child-message="parentMessage" /> -->
-    <Child />
+    <Child :child-message="parentMessage" />
+    <!-- <Child /> -->
+    <Sectors :sectors="sectors" />
   </div>
 </template>
 
 <script>
 import Child from "@/components/Child.vue";
-
+import axios from "axios";
+import Sectors from "@/components/Sectors.vue";
 export default {
   components: {
     Child,
+    Sectors,
   },
   data: function () {
     return {
-      parentMessage: "",
+      parentMessage: "Herllo",
+      sectors: [],
     };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/sectors").then((response) => {
+      this.sectors = response.data;
+      console.log(response.data);
+    });
   },
 };
 </script>
