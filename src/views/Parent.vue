6c0e4d19d9ac8d3f40660e6c15a51dd548b5e2a0
Ian Boyle <irboyle21@gmail.com>
2021-12-27 09:17:14 -0700

    adds parent vue file and child component. adds props to child

---
+++ src/views/Parent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div><Child :message="'Hello'" /></div>
+</template>
+
+<script>
+import Child from "@/components/Child.vue";
+
+export default {
+  components: {
+    Child,
+  },
+  data: function () {
+    return {
+      text: "",
+      message: "",
+    };
+  },
+};
+</script>
